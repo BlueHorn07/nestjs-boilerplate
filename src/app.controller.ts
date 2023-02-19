5f19b91a892f3c339af1f017adc1b98b40e8bfc4
bluehorn07 <bluehornblues@gmail.com>
2023-02-19 21:41:08 +0900

    add database health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/health/database')
+  getDatabaseHello(): Promise<string> {
+    return this.appService.showTables();
+  }
 }
