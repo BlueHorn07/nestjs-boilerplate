efa8558b34b9ebd72cb57f964fc7b56f38a509b9
bluehorn07 <bluehornblues@gmail.com>
2023-03-03 00:35:23 +0900

    update commands

--- codedeploy/deploy.sh
+++ codedeploy/deploy.sh
@@ -7,4 +7,12 @@ ECR_REGISTRY=873251151111.dkr.ecr.ap-northeast-2.amazonaws.com
 ECR_REPOSITORY=nestjs-boilerplate
 
 echo "> Do AWS Codedeploy!"
+
+docker --version
+aws ecr get-login-password --region ap-northeast-2 | \
+  docker login --username AWS --password-stdin 873251151111.dkr.ecr.ap-northeast-2.amazonaws.com
 docker pull $ECR_REGISTRY/$ECR_REPOSITORY:latest
+docker image tag $ECR_REGISTRY/$ECR_REPOSITORY:latest $ECR_REPOSITORY:latest
+
+docker-compose version
+docker-compose -f docker-compose.prod.yaml up -d
