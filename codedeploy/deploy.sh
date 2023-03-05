09b9b322cf3083d35f8c5aa025c84f1a1bc2b61f
bluehorn07 <bluehornblues@gmail.com>
2023-03-05 19:32:17 +0900

    final phase

--- codedeploy/deploy.sh
+++ codedeploy/deploy.sh
@@ -10,13 +10,11 @@ echo "> Do AWS Codedeploy!"
 
 docker --version
 aws ecr get-login-password --region ap-northeast-2 | \
-  docker login --username AWS --password-stdin 873251151111.dkr.ecr.ap-northeast-2.amazonaws.com
+  docker login --username AWS --password-stdin $ECR_REGISTRY
 docker pull $ECR_REGISTRY/$ECR_REPOSITORY:latest
 docker image tag $ECR_REGISTRY/$ECR_REPOSITORY:latest $ECR_REPOSITORY:latest
 
-docker run -d -p 4000:4000 $ECR_REPOSITORY:latest
-
-# docker-compose version
-# docker-compose -f docker-compose.prod.yaml up -d
+docker-compose version
+docker-compose -f docker-compose.dev.yaml up -d
 
 docker image prune -f
