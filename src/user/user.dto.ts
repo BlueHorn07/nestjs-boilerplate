2f3cf9099045a4e47386f08ec8d734df0d7b5e5f
bluehorn07 <bluehornblues@gmail.com>
2023-03-05 22:32:54 +0900

    update swagger

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,20 +1,38 @@
 import { UserType } from './user.type';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserCreateDto {
+  @ApiProperty()
   readonly email: string;
+
+  @ApiProperty()
   readonly id: string;
+
+  @ApiProperty()
   readonly password: string;
+
+  @ApiProperty()
   readonly nickname: string;
+
+  @ApiProperty({ enum: UserType })
   readonly user_type: UserType;
 }
 
 export class UserUpdateDto {
+  @ApiProperty()
   readonly email: string;
+
+  @ApiProperty()
   readonly id: string;
+
+  @ApiProperty()
   readonly nickname: string;
+
+  @ApiProperty({ enum: UserType })
   readonly user_type: UserType;
 }
 
 export class UserPasswordUpdateDto {
+  @ApiProperty()
   readonly password: string;
 }
